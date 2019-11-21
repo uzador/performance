9281fc706131bbf444dd376bd1ea93c85378015b
uzador <uzadorozhny@gmail.com>
2019-11-21 19:39:14 +0300

    Add formatting

--- ajax/ajax_lib.js
+++ ajax/ajax_lib.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
+    const months = ["ЯНВАРЯ", "ФЕВРАЛЯ", "МАРТА","АПРЕЛЯ", "МАЯ", "ИЮНЯ", "ИЮЛЯ", "АВГУСТА", "СЕНТЯБРЯ", "ОКТЯБРЯ", "НОЯБРЬ", "ДЕКАБРЯ"];
+
     var id = 1
-    var weekCount = 1
-    var url = 'http://typicon.online/Schedule/get/' + id + '/' + weekCount;
+    var weekCount = 2
+    var url = 'http://typicon.online/Schedule/get/' + id + '/' + weekCount
 
     $.ajax({
         url: url,
        	type: 'GET',
-    }).done(function(data) {
-        var schedule = JSON.stringify(data, null, '  ')
-	    console.log("OK! " + schedule);
+    }).done(function(jsonResp) {
+//        var schedule = JSON.stringify(data, null, '  ')
+        var schedule = JSON.stringify(jsonResp)
+
+        jsonResp.schedule.forEach(function(value) {
+            console.log(value.Value.Name.Text);
+
+            value.Value.Days.forEach(function(day) {
+                var wDate = new Date(day.Date)
+                var wDateFormatted = wDate.getDate() + "-" + months[wDate.getMonth()] + "-" + wDate.getFullYear()
+
+                console.log("  " + wDateFormatted);
+                console.log("  " + day.Name.Text);
+                console.log("  " + "Знак службы: " + day.SignName.Text);
+
+                day.Worships.forEach(function(worship) {
+                    console.log("    " + worship.Time + " " + worship.Name.Text.Text);
+                });
+            })
+        });
+
 	    $("#schedule").css({"white-space": "pre-wrap"}).text(schedule);
-    }).fail(function(data) {
-        console.log("NO! " + JSON.stringify(data, null, '  '));
+    }).fail(function(jsonResp) {
+        console.log("NO! " + JSON.stringify(jsonResp, null, '  '));
     });
 }); 
